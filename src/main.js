91ec105887d0e9c72d59c94d516e99ffb7399eb6
litury <litury@icloud.com>
2023-11-21 14:35:58 +0500

    ✨🔧 Import and add Font Awesome icons to library in main.js
    This commit adds the necessary code to import and add Font Awesome icons to the library in the main.js file. It imports the fontawesome core, the FontAwesomeIcon component, and specific icons (faChevronDown). The icons are then added to the library using the library.add() method. Additionally, this commit uses the pinia plugin and mounts the app component.

--- src/main.js
+++ src/main.js
@@ -5,8 +5,22 @@ import { createPinia } from 'pinia'
 import App from './App.vue'
 
 
+/* import the fontawesome core */
+import { library } from '@fortawesome/fontawesome-svg-core'
+
+/* import font awesome icon component */
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+
+/* import specific icons */
+import { faChevronDown } from '@fortawesome/free-solid-svg-icons'
+
+/* add icons to the library */
+library.add(faChevronDown)
+
+
 const pinia = createPinia()
 const app = createApp(App)
 
-app.mount('#app')
 app.use(pinia)
+app.component('font-awesome-icon', FontAwesomeIcon)
+app.mount('#app')
