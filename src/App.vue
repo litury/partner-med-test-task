4843dc3ba3b848cdab5b35eac2062bc20b74e009
litury <litury@icloud.com>
2023-11-21 14:32:45 +0500

    ✨:sparkles: feat(App.vue): Update component imports
    This commit updates the component imports in the `App.vue` file. The
    `PanelComponent` import is replaced with `LeftPanel`, and the
    `RightPanel` import is commented out. Additionally, a new import
    `ListDisplay` is added to the file.
    
    The changes can be seen in the diff of the `src/App.vue` file.

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,13 @@
 <template>
-  <PanelComponent />
+  <LeftPanel />
+<!--  <RightPanel />-->
+  <ListDisplay />
 </template>
 
 <script setup>
-import PanelComponent from './components/PanelComponent.vue'
+import LeftPanel from './components/LeftPanel.vue';
+// import RightPanel from './components/RightPanel.vue';
+import ListDisplay from './components/ListDisplay.vue';
 </script>
 
 <style></style>
