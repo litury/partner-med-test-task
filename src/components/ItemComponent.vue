fc1d6bd3cfb3211016cc53c299ce9fe63ca60e61
litury <litury@icloud.com>
2023-11-21 14:33:24 +0500

    ✨ Add ItemComponent.vue
    📝 Add a new component called ItemComponent.vue to the project. The component includes script setup, import statements, props definition, and methods for updating quantity and color. The template contains HTML elements for displaying item information and input fields for checkboxes, quantity, and color. The component also includes scoped CSS for styling the item.

---
+++ src/components/ItemComponent.vue
@@ -0,0 +1,56 @@
+<script setup>
+import { ref, watch } from 'vue';
+import { useListStore } from '../stores/listsStore';
+
+const props = defineProps({
+  item: Object,
+  listId: Number
+});
+
+const listStore = useListStore();
+
+const updateQuantity = () => {
+  listStore.updateQuantity(props.listId, props.item.id, props.item.quantity);
+  console.log('updateQuantity');
+};
+
+const updateColor = () => {
+  listStore.updateColor(props.listId, props.item.id, props.item.color);
+  console.log('updateColor');
+};
+
+</script>
+
+<template>
+  <div class="item">
+    <input type="checkbox" v-model="item.checked" @change="toggleItem" />
+    <span>{{ item.name }}</span>
+    <input type="number" v-model.number="item.quantity" @change="updateQuantity" />
+    <input type="color" :value="item.color" @change="updateColor" />
+  </div>
+</template>
+
+<style scoped>
+.item {
+  display: flex;
+  align-items: center;
+  gap: 10px;
+  margin-bottom: 10px;
+}
+
+.item input[type="checkbox"] {
+  accent-color: #007bff;
+}
+
+.item input[type="number"], .item input[type="color"] {
+  border: 1px solid #ccc;
+  border-radius: 4px;
+  padding: 5px;
+}
+
+.item input[type="color"] {
+  width: 40px;
+  height: 40px;
+  border: none;
+}
+</style>
