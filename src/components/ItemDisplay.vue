e011751e623c83e47153631959c94dd37527a28e
litury <litury@icloud.com>
2023-11-21 14:33:37 +0500

    ✨ feat: Add ItemDisplay component
    The commit adds the ItemDisplay component to the project. The component
    includes a template for displaying an item's name and a dynamic number
    of colored squares.

---
+++ src/components/ItemDisplay.vue
@@ -0,0 +1,37 @@
+<script setup>
+
+const props = defineProps({
+  item: Object
+});
+</script>
+
+<template>
+  <div class="item-display">
+    <span>{{ item.name }}</span>
+
+    <div class="squares">
+      <div v-for="n in item.quantity" :key="n" class="square" :style="{ backgroundColor: item.color }"></div>
+    </div>
+  </div>
+</template>
+
+
+<style>
+.item-display {
+  display: flex;
+  align-items: center;
+  margin-bottom: 10px;
+}
+
+.squares {
+  display: flex;
+  flex-wrap: wrap;
+  margin-left: 10px;
+}
+
+.square {
+  width: 20px;
+  height: 20px;
+  margin: 2px;
+}
+</style>
