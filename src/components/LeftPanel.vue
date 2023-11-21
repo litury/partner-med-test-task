8c7c75460f726db9fc9bae07b9d7cf1ec0127a4a
litury <litury@icloud.com>
2023-11-21 14:34:10 +0500

    ✨ feat(components): Add LeftPanel component
    🎯 Add a new LeftPanel component to the project. The component contains a
    template, script setup, and scoped styles. It renders a div with the class
    "left-panel" and displays a list of ListComponents. The component also
    imports and uses the ListComponent and the listsStore from the stores
    directory. The LeftPanel component has a width of 350px, padding of 20px,
    background color of #fff, box shadow, and border radius of 8px.
    
    Co-authored-by: [author-name] <[author-email]>
    Co-authored-by: [co-author-name] <[co-author-email]>

---
+++ src/components/LeftPanel.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="left-panel">
+    <div v-for="list in lists" :key="list.id">
+
+      <ListComponent :list="list" :collapsed="list.collapsed"/>
+    </div>
+  </div>
+</template>
+
+<script setup>
+import { ref, computed } from 'vue';
+import ListComponent from './ListComponent.vue';
+import { useListStore } from '../stores/listsStore.js';
+
+const store = useListStore();
+const lists = computed(() => store.lists);
+
+const collapsed = ref(true)
+
+const toggleCollapsed = () => {
+  collapsed.value = !collapsed.value
+
+  console.log(collapsed.value);
+}
+
+</script>
+
+<style scoped>
+.left-panel {
+  width: 350px;
+  padding: 20px;
+  background-color: #fff;
+  box-shadow: 0 2px 4px rgba(0,0,0,0.1);
+  border-radius: 8px;
+}
+</style>
