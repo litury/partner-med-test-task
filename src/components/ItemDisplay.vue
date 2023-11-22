c4d267421f528748581dda00d997ca6c062e7536
litury <litury@icloud.com>
2023-11-22 22:04:24 +0500

    ✨✅✨🔀 src/components/ItemDisplay.vue: shuffle squares and remove item
    In this commit, the `ItemDisplay.vue` component is updated to include the following changes:
    
    - Added a button to shuffle the squares.
    - Iterated over the `computedSquares` array to display the squares.
    - Added a click event to remove an item.
    - Refactored the `removeItem` method to remove an item from the list.
    - Created a `key` reference to keep track of changes in order to re-render the component.
    - Implemented the `shuffleSquares` method to shuffle the squares using `Math.random()`.
    
    The component now provides the functionality to shuffle the squares and remove an item.

--- src/components/ItemDisplay.vue
+++ src/components/ItemDisplay.vue
@@ -1,48 +1,85 @@
 <template>
   <div>
-    <div v-if="item.checked" class="item-squares">
-      <div
-          v-for="n in item.quantity"
-          :key="n"
-          class="square"
-          :style="{ backgroundColor: item.color }"
-          @click="removeItem(item.id)"
-      ></div>
-    </div>
+    <button class="button" @click="shuffleSquares">Перемешать</button>
+  </div>
+
+  <div class="item-squares">
+    <div
+      v-for="(square, index) in computedSquares"
+      :key="`${square.itemId}-${index}+${key}`"
+      class="square"
+      :style="{ backgroundColor: square.color }"
+      @click="removeItem(square.itemId)"
+    ></div>
   </div>
 </template>
 
 <script setup>
-import { computed } from 'vue';
-import { useListStore } from '../stores/listsStore.js';
+import { computed, ref, reactive, watch } from "vue";
+import { useListStore } from "../stores/listsStore.js";
 
 const props = defineProps({
-  item: Object,
+  squares: Array,
   listId: Number,
 });
 
 const store = useListStore();
 
+const key = ref(0);
+
 const removeItem = (itemId) => {
   store.removeItem(props.listId, itemId);
+
+  console.log(itemId);
 };
 
+// const displayedSquares = ref(props.squares);
 
+const computedSquares = computed(() => {
+  return props.squares;
+});
+
+// const react = reactive(props.squares)
+//
+// watch(() => props.squares, (newVal, oldVal) => {
+//   console.log("props.squares changed");
+//   console.log(newVal);
+//   console.log(oldVal);
+//
+//   console.log(computedSquares.value);
+// });
+
+const originalSquares = ref({});
+
+const shuffleSquares = () => {
+  originalSquares.value = [...computedSquares.value];
+
+  // перемешиваем массив с помощью функции Math.random()
+  for (let i = computedSquares.value.length - 1; i > 0; i--) {
+    // генерируем случайный индекс
+    const j = Math.floor(Math.random() * (i + 1));
+
+    // меняем местами элементы с индексами i и j
+    [computedSquares.value[i], computedSquares.value[j]] = [
+      computedSquares.value[j],
+      computedSquares.value[i],
+    ];
+
+    key.value++;
+  }
+
+  console.log(computedSquares.value);
+};
 </script>
 
 <style>
-.item-info {
-  display: flex;
-  align-items: center;
-  justify-content: space-between;
-  width: 100%;
-}
 
+.button {
+  margin-bottom: 20px ;
+}
 .item-squares {
   display: flex;
   flex-wrap: wrap;
-  gap: 5px;
-  width: 100%;
 }
 
 .square {
